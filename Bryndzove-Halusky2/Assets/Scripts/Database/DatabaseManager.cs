da1b461975a0937655d997a424f3a0a159cabab3
Phoenix <phoenixmee91@gmail.com>
2018-11-13 21:09:14 +0000

    DB++

--- Bryndzove-Halusky2/Assets/Scripts/Database/DatabaseManager.cs
+++ Bryndzove-Halusky2/Assets/Scripts/Database/DatabaseManager.cs
@@ -4,13 +4,33 @@ using UnityEngine;
 
 public class DatabaseManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    string createAccountURL = "http://kunet.kingston.ac.uk/k1652267/Bryndzove/CreateAccount.php";
+
+    string username = "";
+    string userpass = "";
+
+    // Use this for initialization
+    void Start ()
+    {
+        // testing if database gives right results
+        CreateAccount();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    public void CreateAccount() { StartCoroutine(DB_CreateAccount()); }
+
+    // coroutines
+    // if a stop delay is needed for a reply, fabricate one, it isn't done here
+    IEnumerator DB_CreateAccount()
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("accountName", username);
+        form.AddField("accountPass", userpass);
+        WWW formRequest = new WWW(createAccountURL, form);
+        yield return formRequest;
+
+        if (!string.IsNullOrEmpty(formRequest.error)) Debug.Log("Database error " + formRequest.error);
+        else Debug.Log(formRequest.text.ToString());
+
+        if (formRequest.text.ToString() == "") Debug.Log("LOL");
+    }
 }
